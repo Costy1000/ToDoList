66a22fabd323d8deead78fb140be47f62ad13c9a
Mihai Marvel <costymihai320@gmail.com>
2019-03-14 14:29:40 +0200

    ToDoList

---
+++ script.js
@@ -0,0 +1,92 @@
+ var numberCount  = 0;
+var x;
+
+function create(){
+  	
+  var input = document.getElementById("title-input").value;
+  var inputData = document.getElementById("datepicker").value;
+  if(input != "" && inputData != ""){ ///daca input nu este gol
+	 
+	var create= document.createElement("DIV");
+	var attribute = create.setAttribute("class", "item");
+	
+	  
+	  
+//create H1 element
+    var createH1 = document.createElement("H1");
+	create.appendChild(createH1);
+	var textnode = document.createTextNode(input);
+	createH1.appendChild(textnode);  
+	input = document.getElementById("title-input").value = ""; ///// dupa ce dai click inputul este gol
+	  // create P3 element
+	 var createP3 = document.createElement("P");
+	 var attributeP3 = createP3.setAttribute("class", "item3");
+	 create.appendChild(createP3);
+	 //create P element
+	 var createP = document.createElement("P");
+	 var attributeP = createP.setAttribute("class", "item2");
+	 create.appendChild(createP);
+	 
+	 x = numberCount++;
+	
+	 
+	  
+         
+		
+	  
+	  
+	document.body.appendChild(create);
+	myFunction2();
+	
+  }
+	else{
+		alert("Input must be filled out")
+	}
+	
+	
+}
+ //==============================================================Function2============================================
+
+function myFunction2(){
+                        
+	
+	   var inputData = document.getElementById("datepicker").value;
+	
+	   var hourIndex = document.getElementById("hour").selectedIndex;
+	   var hourOptions = document.getElementById("hour").options;
+	   var minutesIndex = document.getElementById("minutes").selectedIndex;
+	  var minutesOptions = document.getElementById("minutes").options;
+	  var hourText = hourOptions[hourIndex].text;  
+	  var minutesText = minutesOptions[minutesIndex].text; 
+        var newCountDownDate = new Date( inputData + " "  + hourText + ":" + minutesText).getTime();
+	     var item3 =  document.getElementsByClassName("item3")[x].innerHTML =" Time Date: " +  inputData +  " " + hourText + ":" + minutesText;
+	    inputData = document.getElementById("datepicker").value = "";
+	    hourOptions = document.getElementById("hour").selectedIndex= "00";
+	    minutesOptions = document.getElementById("minutes").selectedIndex= "00";
+	var item2 = document.getElementsByClassName("item2")[x];
+	/*inputData = document.getElementById("datepicker").value= "";*/
+setInterval(function(){
+	
+	 var now = new Date();
+
+	var distance  = newCountDownDate - now;
+	
+	  var    days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       var   hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		item2.innerHTML  = "Time left: " +  days + "d "  + hours + "h " + minutes + "m " + seconds +  "s";
+	    
+
+		if(distance < 0){
+		item2.innerHTML = "Expired";
+		}
+	   
+		},1000);
+	  
+	}
+	
+
+
+	
